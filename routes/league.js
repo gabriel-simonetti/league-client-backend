830a2a172ae939bd2ab2dc33d016abb89c68cc79
Gabriel Simonetti <simonettigo@gmail.com>
2018-09-24 01:24:05 -0300

    Fixed error response

--- routes/league.js
+++ routes/league.js
@@ -10,8 +10,8 @@ router.get('/summoner/:summonerName/:serverName', function(req, res) {
   getSummoner(serverName, summonerName)
     .then(summoner => res.json(summoner))
     .catch(err => {
-      res.status(err.statusCode);
-      res.json(JSON.parse(err.error.error.status.message));
+      res.status(err.statusCode ? err.statusCode : 404);
+      res.json("Could not get Summoner data");
     });
 });
 
@@ -23,8 +23,8 @@ router.get('/matches/:accountId/:serverName', function(req, res) {
   getMatchesBySummoner(serverName, accountId)
     .then(matches => res.json(matches))
     .catch(err => {
-      res.status(err.statusCode);
-      res.json(JSON.parse(err.error.error.status.message));
+      res.status(err.statusCode ? err.statusCode : 404);
+      res.json("Could not get Match List data");
     });
 });
 
@@ -35,8 +35,8 @@ router.get('/match/:matchId/:serverName', function(req, res) {
   getMatch(serverName, matchId)
     .then(match => res.json(match))
     .catch(err => {
-      res.status(err.statusCode);
-      res.json(JSON.parse(err.error.error.status.message));
+      res.status(err.statusCode ? err.statusCode : 404);
+      res.json("Could not get Match data");
     });
 });
 
